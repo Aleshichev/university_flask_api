6c3d61e033a2a1faf50bf098814e4ac892cdfec0
Aloshychev Ihor <hiranyagarbha@i.ua>
2022-10-14 15:46:32 +0300

    data

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from handlers.generate_data import random_group, courses, random_students
+import click
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/university'
@@ -19,7 +21,7 @@ class Group(db.Model):
         self.name = name
 
     def __repr__(self):
-        return f"<group: {self.id}>"
+        return f"<group: {self.name}>"
 
 
 class Student(db.Model):
@@ -28,13 +30,13 @@ class Student(db.Model):
     first_name = db.Column(db.String(50), nullable=True)
     last_name = db.Column(db.String(50), nullable=True)
 
-    def __init__(self, name):
-        self.group_id = 'group_id'
-        self.first_name = 'first_name'
-        self.last_name = 'last_name'
+    # def __init__(self, name):
+    #     self.group_id = 'group_id'
+    #     self.first_name = 'first_name'
+    #     self.last_name = 'last_name'
 
     def __repr__(self):
-        return f"<student: {self.id}>"
+        return f"<student: {self.group_id}>"
 
 
 class Course(db.Model):
@@ -42,16 +44,43 @@ class Course(db.Model):
     name = db.Column(db.String(50), unique=True, primary_key=True)
     description = db.Column(db.String(500), nullable=True)
 
-    def __init__(self, name):
-        self.name = 'name'
-        self.description = 'description'
+    # def __init__(self, name):
+    #     self.name = 'name'
+    #     self.description = 'description'
 
     def __repr__(self):
-        return f"<course: {self.id}>"
+        return f"<course: {self.name}{self.description}>"
 
-#
-# with app.app_context():
-#     db.create_all()
+
+@app.cli.command("add_data_group")
+def add_data_group():
+    group_list = random_group()
+    for i in range(len(group_list)):
+        new_group = Group(name=group_list[i])
+        db.session.add(new_group)
+        db.session.commit()
+
+
+@app.cli.command("add_data_courses")
+def add_data_courses():
+    for i in range(len(courses)):
+        new_course = Course(name=courses[i], description="some_description")
+        db.session.add(new_course)
+        db.session.commit()
+
+
+@app.cli.command("add_data_student")
+def add_data_student():
+    students_list = random_students()
+    for i in range(len(students_list)):
+        name = students_list[i].split(" ")[0]
+        l_name = students_list[i].split(" ")[1]
+        number = 1 + i
+        new_course = Student(group_id=number, first_name=name,
+                             last_name=l_name)
+        db.session.add(new_course)
+        db.session.commit()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ src/handlers/generate_data.py
@@ -0,0 +1,54 @@
+import random
+import string
+from random import choice, randint, shuffle
+import names
+
+"""
++ 10 групп со случайно сгенерированными именами. Имя должно содержать 2 символа, дефис, 2 цифры
++ Создать 10 курсов (математика, биология и т. д.)
++ 200 студентов. Возьмите 20 имен и 20 фамилий и случайным образом скомбинируйте их, чтобы получить учеников.
+4. Случайным образом распределите учащихся по группам. В каждой группе могло быть от 10 до 30 студентов. Возможно, что какие-то группы будут без студентов или студенты без групп
+5. Создайте отношение MANY-TO-MANY между таблицами STUDENTS и COURSES. Случайным образом назначить от 1 до 3 курсов для каждого студента
+
+"""
+courses = ["Matematic", "Algebra", "Biology", "Drawing", "Chemistry", "Geography", "Geometry", "History", "Literature",
+           "Physics"]
+
+
+def random_letter():
+    letter = choice(string.ascii_lowercase)
+    return letter
+
+
+def random_naumber():
+    number = choice(string.digits)
+    return number
+
+
+def random_group():
+    group_list = []
+    for i in range(10):
+        group_list.append(f"{random_letter()}{random_letter()} - {random_naumber()}{random_naumber()}")
+    return group_list
+
+
+def first_name():
+    first_name_list = []
+    for i in range(10):
+        first_name_list.append(f"{names.get_first_name(gender='female')}")
+        first_name_list.append(f"{names.get_first_name(gender='male')}")
+    return first_name_list
+
+
+def last_name():
+    last_name_list = []
+    for i in range(20):
+        last_name_list.append(f"{names.get_last_name()}")
+    return last_name_list
+
+
+def random_students():
+    students_list = []
+    for i in range(200):
+        students_list.append(f"{random.choice(first_name())} {random.choice(last_name())}")
+    return students_list
